3c60286fcdedf1ac6061fdbe564694ebced825c6
agumusay <a.gumusay@protonmail.com>
2020-02-24 09:53:09 +0100

    clean

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,8 @@
 import "../styles/main.scss";
 import $ from "jquery";
-import "bootstrap/dist/js/bootstrap.bundle";
 import "popper.js/dist/popper";
 import "bootstrap";
-import "../../node_modules/bootstrap/scss/bootstrap.scss";
+import "bootstrap/scss/bootstrap.scss";
 
 $(function() {
   $('[data-toggle="popover"]').hover(
